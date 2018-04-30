4c132571e033db906ff312752b0fe37a2a2ee9f8
James Gayfer <gayfer.james@gmail.com>
2018-04-29 21:20:09 -0700

    Add in memory database to rspec
    An in memory database is fast, and allows for a clean slate every time
    the test suite is run.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,1 +1,13 @@
-require 'rspec'
+$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+require 'simplecov'
+
+SimpleCov.start do
+  minimum_coverage(100)
+end
+
+require 'discordrb'
+require 'active_record'
+
+ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: ':memory:')
+ActiveRecord::Schema.verbose = false
+load "db/schema.rb"
