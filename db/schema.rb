3c5e3537fb28e85211f754ff23402355a73af808
James Gayfer <gayfer.james@gmail.com>
2019-06-05 22:09:50 -0700

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180514202245) do
+ActiveRecord::Schema.define(version: 2018_05_14_202245) do
 
   create_table "live_matches", force: :cascade do |t|
     t.integer "match_id"
